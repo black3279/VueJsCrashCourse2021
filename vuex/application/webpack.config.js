cf887082af5573bf91ba9ecafc44c8fc61322ad9
black3279 <30133279@naver.com>
2021-07-04 22:38:25 +0900

    20210704

--- vuex/application/webpack.config.js
+++ vuex/application/webpack.config.js
@@ -1,78 +1,36 @@
-var path = require('path')
-var webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+  devtool: 'eval',
+  mode: 'development',
+  resolve: {
+    extensions: ['.vue', '.js'],
+  },
+  entry: {
+    app: [path.join(__dirname, 'TicTacToe.vue')],
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+    }, {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+      ],
+    },],
   },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist'
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+};
